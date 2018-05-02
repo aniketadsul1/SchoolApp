c83c8ee933d1886bdb2566df0aca74592dcee9f6
aniketadsul <aniketadsul@sudesi.in>
2018-05-02 16:11:16 +0530

    fragment a changes done by Aniket

--- app/src/main/java/com/android/sudesi/schoolapp/FragmentA.java
+++ app/src/main/java/com/android/sudesi/schoolapp/FragmentA.java
@@ -1,112 +1,158 @@
 package com.android.sudesi.schoolapp;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
-import com.android.sudesi.schoolapp.Activity.StudentActivity;
-import com.android.sudesi.schoolapp.dbconfig.DbHelper;
-import com.android.sudesi.schoolapp.libs.Utils;
-import com.android.sudesi.schoolapp.model.StudentDetailModel;
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentA extends Fragment {
-
-    private EditText edt_roll_no, edt_name, edt_dob, edt_address, edt_Parent_Mob_no, edt_std, edt_div;
-    private Button btn_save;
-    private String str_rollNo, str_name, str_dob, str_address, str_parent_MobNo, str_std, str_div;
-    protected Utils utils;
+    Spinner spin_standard,spin_division;
+    ImageView ivImage;
     private Context mContext;
-    private StudentDetailModel studentDetailModel;
-    private List<StudentDetailModel> studentDetailModelList;
-    private List<String> studentDetailsArrayList;
-    DbHelper dbHelper;
-
+    private List<String> StudentDetailsArrayList;
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private String userChoosenTask;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = getActivity();
-        utils = new Utils(mContext);
-        studentDetailModelList = new ArrayList<>();
-        studentDetailsArrayList = new ArrayList<>();
-        dbHelper = new DbHelper(mContext);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_a, container, false);
-
-        mContext = getActivity();
-        // Inflate the layout for this fragment
+        View rootView=inflater.inflate(R.layout.fragment_a, container, false);
         init(rootView);
         return rootView;
+    }
+    private void init(View rootview){
+        StudentDetailsArrayList=new ArrayList<>();
+        spin_standard=(Spinner)rootview.findViewById(R.id.spin_standard);
+        spin_division=(Spinner)rootview.findViewById(R.id.spin_division);
+        ivImage=(ImageView)rootview.findViewById(R.id.ivImage);
 
+        ivImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
 
     }
 
-    private void init(View rootView) {
 
-        //EditText
-        edt_roll_no = (EditText)rootView.findViewById(R.id.edt_roll_no);
-        edt_name = (EditText) rootView.findViewById(R.id.edt_name);
-        edt_dob = (EditText)rootView.findViewById(R.id.edt_dob);
-        edt_address = (EditText)rootView.findViewById(R.id.edt_address);
-        edt_Parent_Mob_no = (EditText)rootView.findViewById(R.id.edt_Parent_Mob_no);
-        edt_std = (EditText)rootView.findViewById(R.id.edt_std);
-        edt_div = (EditText)rootView.findViewById(R.id.edt_div);
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
 
-        //Button
-        btn_save = (Button)rootView.findViewById(R.id.btn_save);
-
-        btn_save.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try {
-                    str_rollNo = edt_roll_no.getText().toString().trim();
-                    str_name = edt_name.getText().toString().trim();
-                    str_dob = edt_dob.getText().toString().trim();
-                    str_address = edt_address.getText().toString().trim();
-                    str_parent_MobNo = edt_Parent_Mob_no.getText().toString().trim();
-                    str_std = edt_std.getText().toString().trim();
-                    str_div = edt_div.getText().toString().trim();
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result=Utility.checkPermission(getContext());
 
-                    if (!TextUtils.isEmpty(edt_roll_no.getText().toString().trim()) && !TextUtils.isEmpty(edt_name.getText().toString().trim()) &&
-                            !TextUtils.isEmpty(edt_dob.getText().toString().trim()) && !TextUtils.isEmpty(edt_address.getText().toString().trim()) &&
-                            !TextUtils.isEmpty(edt_Parent_Mob_no.getText().toString().trim()) && !TextUtils.isEmpty(edt_std.getText().toString().trim()) &&
-                            !TextUtils.isEmpty(edt_div.getText().toString().trim())) {
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask ="Take Photo";
+                    if(result)
+                        cameraIntent();
 
-                        String valuesArray[] = {str_rollNo, str_name, str_dob, str_address, str_parent_MobNo, str_std, str_div};
-                        String[] selectionArgs = {str_rollNo};//
-                        String selection = "Roll_No" + " = ?";
-                        boolean result = SchoolApp.dbCon.updateBulk(DbHelper.TABLE_DB_SCHOOL, selection, valuesArray, utils.column_studentDetails, selectionArgs);
-                        if (result) {
-                            Toast.makeText(mContext, "Data inserted Successfully", Toast.LENGTH_SHORT).show();
-                            edt_roll_no.setText("");
-                            edt_name.setText("");
-                            edt_dob.setText("");
-                            edt_address.setText("");
-                            edt_Parent_Mob_no.setText("");
-                            edt_std.setText("");
-                            edt_div.setText("");
-                        }
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask ="Choose from Library";
+                    if(result)
+                        galleryIntent();
 
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
                 }
-
             }
         });
+        builder.show();
+    }
+
+
+    private void galleryIntent()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
+    }
+
+    private void cameraIntent()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ivImage.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm=null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ivImage.setImageBitmap(bm);
     }
 
 
