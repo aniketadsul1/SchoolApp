d9a086065d82fbd43d4f4f9aad5ebbb7b4126069
aniketadsul <aniketadsul@sudesi.in>
2018-05-03 12:06:32 +0530

    attendace fragment done

--- app/src/main/java/com/android/sudesi/schoolapp/FragmentE.java
+++ app/src/main/java/com/android/sudesi/schoolapp/FragmentE.java
@@ -26,7 +26,7 @@ import android.view.ViewGroup;
 public class FragmentE extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { 
 
         return inflater.inflate(R.layout.fragment_e, container, false);
     }
